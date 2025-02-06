e90b64121cc684eec99734d531cdc424fc2e383f
arsweng <mugishawenge1@gmail.com>
2025-02-06 09:15:22 +0200

    added the profile function and routes

--- backend/routes/AuthRoutes.js
+++ backend/routes/AuthRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const { signup, login, signupAdmin, loginAdmin } = require('../controllers/authController')
+const { signup, login, signupAdmin, loginAdmin, profile } = require('../controllers/authController')
 const router = express.Router()
 
 router.post('/signup', signup)
 router.post('/login', login)
-
+router.get('/profile',profile)
 router.post('/signupAdmin', signupAdmin)
 router.post('/loginAdmin', loginAdmin)
 
